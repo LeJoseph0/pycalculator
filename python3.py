95d1722edbcf2e235227dade6f5424516cf3979b
LeJoseph0 <rippedjo2023@gmail.com>
2023-06-24 15:36:25 +0200

    calculator v1

---
+++ python3.py
@@ -0,0 +1,30 @@
+while True:  
+  # input first number
+try:
+first_number = float(input("enter first number: "))
+except ValueError:
+print("invalid input. please enter a number.")
+  
+  # input operation type
+operation = input("enter operation type: ")
+  
+  # input second number
+second_number = float(input("enter second number: "))
+  
+  # print the result
+  
+  if operation == "+":
+    print(first_number + second_number)
+  elif operation == "-":
+    print(first_number - second_number)
+  elif operation == "/":
+    print(first_number / second_number)
+  elif operation == "*":
+    print(first_number * second_number)
+  else:
+    print("error")
+
+  repeat = input("do you want to preforme another operation? (y/n): ")
+  if repeat == "n":
+    break
+print("calculator closed..")
