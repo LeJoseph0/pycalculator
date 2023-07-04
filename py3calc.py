9ea453c871603fd6f3601e747d4f3f1fec807662
LeJoseph0 <rippedjo2023@gmail.com>
2023-07-04 17:33:22 +0200

    check if second input is a number

--- py3calc.py
+++ py3calc.py
@@ -1,35 +1,38 @@
-while True:  
-  # input first number
-  while True:
-    try:
-      first_number = float(input("enter first number: "))
-      break
-    except ValueError: 
-      print("invalid input, enter a number.")
+while True:
+    # input first number
+    while True:
+        try:
+            first_number = float(input("enter first number: "))
+            break
+        except ValueError:
+            print("invalid input, enter a numeric value")
 
+    # input operation type
+    operation = input("enter operation type: ")
 
-  # input operation type
-  operation = input("enter operation type: ")
-  
-  # input second number
-  second_number = float(input("enter second number: "))
-  
-  # print the result
-  
-  if operation == "+":
-    print(first_number + second_number)
-  elif operation == "-":
-    print(first_number - second_number)
-  elif operation == "/":
-    print(first_number / second_number)
-  elif operation == "*":
-    print(first_number * second_number)
-  else:
-    print("error")
+    # input second number
+    while True:
+        try:
+            second_number = float(input("enter second number: "))
+            break
+        except ValueError:
+            print("invalid input, enter a numeric value")
 
-  repeat = input("do you want to preforme another operation? (y/n): ")
-  if repeat == "n":
-    break
+    # print the result
+
+    if operation == "+":
+        print(first_number + second_number)
+    elif operation == "-":
+        print(first_number - second_number)
+    elif operation == "/":
+        print(first_number / second_number)
+    elif operation == "*":
+        print(first_number * second_number)
+    else:
+        print("error")
+
+    repeat = input("do you want to preforme another operation? (y/n): ")
+    if repeat == "n":
+        break
 
 print("calculator closed..")
-
